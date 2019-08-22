6f0e550db9df7b5345bd808823f3e2b48fcfd6bc
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-08-22 17:35:25 -0500

    Updates

---
+++ precache-manifest.6527471de2a5a58d9ec7831304a6409c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6758b31badc3a1ca0c5f4afd635c63e6",
+    "url": "/lp0514/index.html"
+  },
+  {
+    "revision": "c4adcee95cb1ab95270a",
+    "url": "/lp0514/static/css/2.2f374adb.chunk.css"
+  },
+  {
+    "revision": "1c9689216c6900921983",
+    "url": "/lp0514/static/css/main.4df74037.chunk.css"
+  },
+  {
+    "revision": "c4adcee95cb1ab95270a",
+    "url": "/lp0514/static/js/2.4590310d.chunk.js"
+  },
+  {
+    "revision": "1c9689216c6900921983",
+    "url": "/lp0514/static/js/main.af03d955.chunk.js"
+  },
+  {
+    "revision": "e5b8e95c7a496e548b57",
+    "url": "/lp0514/static/js/runtime~main.189e5b54.js"
+  }
+]);
